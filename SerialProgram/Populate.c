1413c38b6a318bb194f2f1987d2a8cbb69b15a0d
BlebaSbluzu <146071033+BlebaSbluzu@users.noreply.github.com>
2024-11-14 13:32:54 +0000

    Initial commit

---
+++ SerialProgram/Populate.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+int randCol = rand()*COLS;
+int randRow = rand()*ROWS;
+
+int Current[ROWS, COLS];
+int Future[ROWS, COLS];
+
+for(i=0,i<ROWS,i++){
+  for(z=0,z<COLS, z++){
+    Current[i
+  }
+ }
